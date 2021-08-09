45396d61e950b0ef9288dc765d51dcdd43daa41b
SUNGHO AHN <jugiaro95@gmail.com>
2021-08-09 21:42:22 +0900

    rename folder

---
+++ 2021 포스코ICT/poscoict-pjt/src/main/java/com/poscoict/poscoict/pjt/PoscoictPjtApplication.java
@@ -0,0 +1,13 @@
+package com.poscoict.poscoict.pjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoscoictPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoscoictPjtApplication.class, args);
+	}
+
+}
---
+++ 2021 포스코ICT/poscoict-pjt/src/test/java/com/poscoict/poscoict/pjt/PoscoictPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poscoict.poscoict.pjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoscoictPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
