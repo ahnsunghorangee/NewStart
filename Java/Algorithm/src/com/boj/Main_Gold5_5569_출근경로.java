e139a8a070825f3cbebe6a44b63785720fa222f4
SUNGHO AHN <jugiaro95@gmail.com>
2021-09-08 23:12:16 +0900

    :x:

---
+++ Java/Algorithm/src/com/boj/Main_Gold5_5569_출근경로.java
@@ -0,0 +1,34 @@
+package com.boj;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_Gold5_5569_출근경로 {
+	
+	static int W, H;
+	static int[][][][] dp;
+	static final int mod = 100_000;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		W = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+		
+		dp = new int[W+1][H+1][2][2];
+		
+
+		bw.write(sb.toString());
+        bw.flush();
+
+        br.close(); 
+        bw.close();
+	}
+}
